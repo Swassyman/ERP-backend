0f4eb22d2a58db212b04a5988768ff179628d6bd
Swassyman <swassyman2005@gmail.com>
2026-02-22 19:17:23 +0530

    chore: format fixes

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -1,130 +1,130 @@
 import { relations, sql } from "drizzle-orm";
 import {
-    type AnyPgColumn,
-    bigint,
-    boolean,
-    check,
-    integer,
-    pgEnum,
-    pgTable,
-    smallint,
-    text,
-    timestamp,
-    unique,
+	type AnyPgColumn,
+	bigint,
+	boolean,
+	check,
+	integer,
+	pgEnum,
+	pgTable,
+	smallint,
+	text,
+	timestamp,
+	unique,
 } from "drizzle-orm/pg-core";
 
 // Enums
 
 export const organizationTypeEnum = pgEnum("organization_type", [
-    "department",
-    "club",
-    "institution",
+	"department",
+	"club",
+	"institution",
 ]);
 
 // Common fields
 const commonFields = {
-    createdAt: timestamp({ mode: "string", withTimezone: true })
-        .defaultNow()
-        .notNull(),
-    updatedAt: timestamp({ mode: "string", withTimezone: true })
-        .defaultNow()
-        .$onUpdate(() => sql`now()`)
-        .notNull(),
+	createdAt: timestamp({ mode: "string", withTimezone: true })
+		.defaultNow()
+		.notNull(),
+	updatedAt: timestamp({ mode: "string", withTimezone: true })
+		.defaultNow()
+		.$onUpdate(() => sql`now()`)
+		.notNull(),
 };
 
 // Tables
 
 export const organization = pgTable(
-    "organization",
-    {
-        id: integer().primaryKey().generatedAlwaysAsIdentity(),
-        name: text().notNull(),
-        type: organizationTypeEnum().notNull(),
-        parentOrganizationId: integer().references(
-            (): AnyPgColumn => organization.id,
-        ),
-        isActive: boolean().notNull().default(true),
-        deletedAt: timestamp({ mode: "string", withTimezone: true }),
-        ...commonFields,
-    },
-    (t) => [unique().on(t.name)],
+	"organization",
+	{
+		id: integer().primaryKey().generatedAlwaysAsIdentity(),
+		name: text().notNull(),
+		type: organizationTypeEnum().notNull(),
+		parentOrganizationId: integer().references(
+			(): AnyPgColumn => organization.id,
+		),
+		isActive: boolean().notNull().default(true),
+		deletedAt: timestamp({ mode: "string", withTimezone: true }),
+		...commonFields,
+	},
+	(t) => [unique().on(t.name)],
 );
 
 export const user = pgTable(
-    "user",
-    {
-        id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
-        fullName: text().notNull(),
-        email: text().notNull(),
-        passwordHash: text().notNull(),
-        isActive: boolean().notNull().default(true),
-        deletedAt: timestamp({ mode: "string", withTimezone: true }),
-        ...commonFields,
-    },
-    (t) => [
-        unique().on(t.email),
-        check("email_check", sql`${t.email} LIKE '%@tkmce.ac.in'`),
-    ],
+	"user",
+	{
+		id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
+		fullName: text().notNull(),
+		email: text().notNull(),
+		passwordHash: text().notNull(),
+		isActive: boolean().notNull().default(true),
+		deletedAt: timestamp({ mode: "string", withTimezone: true }),
+		...commonFields,
+	},
+	(t) => [
+		unique().on(t.email),
+		check("email_check", sql`${t.email} LIKE '%@tkmce.ac.in'`),
+	],
 );
 
 export const userRelations = relations(user, (r) => ({
-    organizationRoles: r.many(organizationUserRole),
+	organizationRoles: r.many(organizationUserRole),
 }));
 
 export const role = pgTable(
-    "role",
-    {
-        id: smallint().primaryKey().generatedAlwaysAsIdentity(),
-        roleName: text().notNull(),
-        ...commonFields,
-    },
-    (t) => [unique().on(t.roleName)],
+	"role",
+	{
+		id: smallint().primaryKey().generatedAlwaysAsIdentity(),
+		roleName: text().notNull(),
+		...commonFields,
+	},
+	(t) => [unique().on(t.roleName)],
 );
 
 export const roleRelations = relations(role, (r) => ({
-    organizationUsers: r.many(organizationUserRole),
+	organizationUsers: r.many(organizationUserRole),
 }));
 
 export const organizationUserRole = pgTable(
-    "organization_user_role",
-    {
-        id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
-        userId: bigint({ mode: "bigint" })
-            .references(() => user.id)
-            .notNull(),
-        roleId: smallint()
-            .references(() => role.id)
-            .notNull(),
-        organizationId: integer()
-            .references(() => organization.id)
-            .notNull(),
-        isActive: boolean().notNull().default(true),
-        deletedAt: timestamp({ mode: "string", withTimezone: true }),
-        ...commonFields,
-    },
-    (t) => [
-        unique()
-            .on(t.userId, t.roleId, t.organizationId, t.deletedAt)
-            .nullsNotDistinct(),
-    ],
+	"organization_user_role",
+	{
+		id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
+		userId: bigint({ mode: "bigint" })
+			.references(() => user.id)
+			.notNull(),
+		roleId: smallint()
+			.references(() => role.id)
+			.notNull(),
+		organizationId: integer()
+			.references(() => organization.id)
+			.notNull(),
+		isActive: boolean().notNull().default(true),
+		deletedAt: timestamp({ mode: "string", withTimezone: true }),
+		...commonFields,
+	},
+	(t) => [
+		unique()
+			.on(t.userId, t.roleId, t.organizationId, t.deletedAt)
+			.nullsNotDistinct(),
+	],
 );
 
 export const organizationUserRoleRelations = relations(
-    organizationUserRole,
-    (r) => ({
-        user: r.one(user, {
-            fields: [organizationUserRole.userId],
-            references: [user.id],
-        }),
-        role: r.one(role, {
-            fields: [organizationUserRole.roleId],
-            references: [role.id],
-        }),
-        organization: r.one(organization, {
-            fields: [organizationUserRole.organizationId],
-            references: [organization.id],
-        }),
-    }),
+	organizationUserRole,
+	(r) => ({
+		user: r.one(user, {
+			fields: [organizationUserRole.userId],
+			references: [user.id],
+		}),
+		role: r.one(role, {
+			fields: [organizationUserRole.roleId],
+			references: [role.id],
+		}),
+		organization: r.one(organization, {
+			fields: [organizationUserRole.organizationId],
+			references: [organization.id],
+		}),
+	}),
 );
 
 // export const permission = pgTable("permission", {
