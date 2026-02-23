704af86705e5bd240e129d157b9331ff310e1fdd
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-02-23 18:38:08 +0530

    minor cleanup: organizing imports, better practices

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,10 @@
 export const INSTITUTION_DOMAIN_REGEXP = /@tkmce\.ac\.in$/;
+<<<<<<< HEAD
 
 export const ORGANIZATION_TYPES = [
 	"department",
 	"club",
 	"institution",
 ] as const;
+=======
+>>>>>>> 6e7e8cb (minor cleanup: organizing imports, better practices)
