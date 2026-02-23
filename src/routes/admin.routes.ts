5e2cb0f251b426099dfe13d786e67aa559c4da31
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-02-23 23:33:33 +0530

    fix: add missing routes

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -3,12 +3,20 @@ import {
 	assignRole,
 	createOrganization,
 	createUser,
+	getOrganizations,
+	getRoles,
+	getUsers,
 } from "../controllers/admin.controller.js";
 
 const router: Router = Router();
 
+router.get("/users", getUsers);
 router.post("/users", createUser);
+
+router.get("/organizations", getOrganizations);
 router.post("/organizations", createOrganization);
+
+router.get("/roles", getRoles);
 router.post("/roles", assignRole);
 
 export default router;
