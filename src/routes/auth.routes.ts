80710286a3effab98eaf89079ebcc917758c8997
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-02-27 03:44:14 +0530

    fix: logout route method

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -6,7 +6,7 @@ const router: Router = express.Router();
 
 router.post("/login", authController.login);
 router.post("/refresh", authController.refresh);
-router.get("/logout", authController.logout);
+router.post("/logout", authController.logout);
 
 router.use(authenticateToken);
 router.get("/me", authController.userDetails);
