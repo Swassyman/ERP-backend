f805c21719f57e55005dd66062122da1922a4261
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-03-03 17:20:47 +0530

    implement permissions routes

--- src/routes/permissions.routes.ts
+++ src/routes/permissions.routes.ts
@@ -3,4 +3,7 @@ import * as controller from "../controllers/permissions.controller.js";
 
 const router: Router = Router();
 
+router.get("/", controller.getPermissions);
+router.get("/:id", controller.getPermission);
+
 export default router;
