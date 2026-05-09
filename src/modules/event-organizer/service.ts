c5bd1135d545f0ae02526facccec7b2357204b1d
Alan <alansaji272@gmail.com>
2026-05-10 00:14:32 +0530

    Event organizer Modules  and added resource_provided to EVENT_ORGANIZER_ROLE

---
+++ src/modules/event-organizer/service.ts
@@ -0,0 +1,45 @@
+import { ConflictError, ForbiddenError, NotFoundError } from "@/lib/errors.js";
+import * as eventRepository from "@/modules/event/repository.js"; //expected in event!
+import * as repository from "./repository.js";
+import type { AddEventOrganizerSchema } from "./schema.js";
+
+export async function getEventOrganizers(eventId: number) {
+	const event = await eventRepository.findEventById(eventId);
+	if (event == null) throw new NotFoundError("Event not found");
+	return await repository.getEventOrganizers(eventId);
+}
+
+export async function addEventOrganizer(eventId: number, input: AddEventOrganizerSchema) {
+	const event = await eventRepository.findEventById(eventId);
+	if (event == null) throw new NotFoundError("Event not found");
+
+	if (event.status !== "draft") {
+		throw new ForbiddenError("Organziers can be added only at draft stage");
+	}
+
+	return await repository.addEventOrganizer({
+		eventId,
+		organizationId: input.organizationId,
+		role: input.role,
+	});
+}
+
+export async function removeEventOrganizer(eventId: number, organizerId: number) {
+	const event = await eventRepository.findEventById(eventId);
+	if (event == null) throw new NotFoundError("Event not found");
+
+	if (event.status !== "draft") {
+		throw new ForbiddenError("Organizers can only be removed during draft stage");
+	}
+
+	const organizer = await repository.findEventOrganizer(eventId, organizerId);
+	if (organizer == null) {
+		throw new NotFoundError("Organizer not found");
+	}
+
+	if (organizer.role === "host") {
+		throw new ConflictError("Cannot remove host of the event");
+	}
+
+	return await repository.removeEventOrganizer(organizerId);
+}
